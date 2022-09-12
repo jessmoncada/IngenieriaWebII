977a7c41af827b91cf6a35460f54bbd78ca774f6
Jessica Moncada <jessmoncada1@gmail.com>
2022-09-12 18:42:29 -0500

    Se agrega put, post y get de tipoEquipos y estado equipos

--- router/estadoEquipo.js
+++ router/estadoEquipo.js
@@ -1,17 +1,57 @@
 const { Router } = require('express');
+const EstadoEquipo = require('../models/EstadoEquipo');
 const router = Router ();
 
 //Crear recursos 
-router.post('/', function(req, res){
-    res.send('Estado de equipo Post');
+router.post('/', async function(req, res){
+    try{
+        let estadoEquipo = new EstadoEquipo(); 
+        estadoEquipo.nombre = req.body.nombre;
+        estadoEquipo.estado = req.body.estado;
+        estadoEquipo.fechaCreacion = new Date();
+        estadoEquipo.fechaActualizacion = new Date ();
+    
+        estadoEquipo = await estadoEquipo.save();
+    
+        res.send(estadoEquipo);
+        
+    } catch(error){
+        console.log(error);
+        res.send('Ocurrió un error');
+    }
 });
 
-router.get('/', function(req, res){
-    res.send('Estado de equipo Get');
+//Listar
+router.get('/', async function(req, res){
+    try{
+        const estadoEquipo = await EstadoEquipo.find();
+        res.send(estadoEquipo)
+    
+      }catch(error){
+        console.log(error);
+        res.send('Ocurrió un error');
+      }
+
 });
 
-router.put('/', function(req, res){
-    res.send('Estado de equipo Put');
+//Actualizar
+router.put('/:estadoEquipoId', async function(req, res){
+    try{
+        let estadoEquipo = await EstadoEquipo.findById(req.params.estadoEquipoId); 
+        if(!estadoEquipo){
+          return res.send('Estado equipo no existe');
+        }
+        estadoEquipo.nombre = req.body.nombre;
+        estadoEquipo.estado = req.body.estado;
+        estadoEquipo.fechaActualizacion = new Date ();
+        estadoEquipo = await estadoEquipo.save();
+    
+        res.send(estadoEquipo);
+        
+    } catch(error){
+        console.log(error);
+        res.send('Ocurrió un error');
+    }
 });
 
 module.exports = router; 
--- router/tipoEquipo.js
+++ router/tipoEquipo.js
@@ -1,17 +1,56 @@
 const { Router } = require('express');
+const TipoEquipo = require ('../models/TipoEquipo');
 const router = Router ();
 
 //Crear recursos 
-router.post('/', function(req, res){
-    res.send('Tipo de equipo Post');
+router.post('/', async function(req, res){
+    try{
+        let tipoEquipo = new TipoEquipo(); 
+        tipoEquipo.nombre = req.body.nombre;
+        tipoEquipo.estado = req.body.estado;
+        tipoEquipo.fechaCreacion = new Date();
+        tipoEquipo.fechaActualizacion = new Date ();
+    
+        tipoEquipo = await tipoEquipo.save();
+    
+        res.send(tipoEquipo);
+        
+    } catch(error){
+        console.log(error);
+        res.send('Ocurrió un error');
+    }
+    
 });
 
-router.get('/', function(req, res){
-    res.send('Tipo de equipo Get');
+router.get('/', async function(req, res){
+    try{
+        const tipoEquipo = await TipoEquipo.find();
+        res.send(tipoEquipo)
+    
+      }catch(error){
+        console.log(error);
+        res.send('Ocurrió un error');
+      }
+
 });
 
-router.put('/', function(req, res){
-    res.send('Tipo de equipo Put');
+router.put('/:tipoEquipoId', async function(req, res){
+    try{
+    let tipoEquipo = await TipoEquipo.findById(req.params.tipoEquipoId); 
+    if(!tipoEquipo){
+      return res.send('Tipo de equipo no existe');
+    }
+    tipoEquipo.nombre = req.body.nombre;
+    tipoEquipo.estado = req.body.estado;
+    tipoEquipo.fechaActualizacion = new Date ();
+    tipoEquipo = await tipoEquipo.save();
+
+    res.send(tipoEquipo);
+    
+} catch(error){
+    console.log(error);
+    res.send('Ocurrió un error');
+}
 });
 
 module.exports = router; 
