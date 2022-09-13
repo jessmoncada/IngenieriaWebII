20d8a65e465e2a21e99554456a810916571c2c8c
Jessica Moncada <jessmoncada1@gmail.com>
2022-09-12 21:03:11 -0500

    Actividad 1 finalizada funcionando ok

--- router/inventario.js
+++ router/inventario.js
@@ -43,20 +43,20 @@ router.get('/', async function(req, res){
         const inventario = await Inventario.find().populate([
         
             {
-                path: 'usuario'
+                path: 'usuario', select: 'nombre email estado'
 
             },
 
             {
-                path: 'marca'
+                path: 'marca', select: 'nombre estado'
 
             },
             {
-                path: 'tipoEquipo'
+                path: 'tipoEquipo', select: 'nombre estado'
 
             },
             {
-                path: 'estadoEquipo'
+                path: 'estadoEquipo', select: 'nombre estado'
 
             },
         ]);
@@ -68,8 +68,42 @@ router.get('/', async function(req, res){
       }
 });
 
-router.put('/', function(req, res){
-    res.send('Inventario Put');
+router.put('/:inventarioId', async function(req, res){
+
+    try{
+        let inventario = await Inventario.findById(req.params.inventarioId); 
+        if(!inventario){
+          return res.send('Inventario no existe');
+        }
+
+        const existeInventarioPorSerial = await Inventario.findOne({serial: req.body.serial, _id: {$ne:inventario._id}}); 
+        if (existeInventarioPorSerial){
+            return res.send('Ya existe el serial para otro equipo');
+        }
+
+        inventario.serial = req.body.serial;
+        inventario.modelo = req.body.modelo;
+        inventario.descripcion = req.body.descripcion;
+        inventario.foto = req.body.foto;
+        inventario.color = req.body.color;
+        inventario.fechaCompra = req.body.fechaCompra;
+        inventario.precio = req.body.precio;
+        inventario.usuario = req.body.usuario._id;
+        inventario.marca = req.body.marca._id;
+        inventario.tipoEquipo = req.body.tipoEquipo._id;
+        inventario.estadoEquipo = req.body.estadoEquipo._id;
+        inventario.fechaCreacion = new Date();
+        inventario.fechaActualizacion = new Date ();
+    
+        inventario = await inventario.save();
+    
+        res.send(inventario);
+        
+    } catch(error){
+        console.log(error);
+        res.send('Ocurrió un error');
+    }
+
 });
 
 module.exports = router; 
